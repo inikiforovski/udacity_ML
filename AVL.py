d295c66428992a47f84af832f38d010da12b4911
Igor N <inikiforovski@gmail.com>
2018-09-14 11:16:03 -0400

    added BFS

--- AVL.py
+++ AVL.py
@@ -1,4 +1,6 @@
 import math
+import Queue
+import numpy as np
 
 
 class AVLNode:
@@ -10,6 +12,45 @@ class AVLNode:
         self.right = None
         self.height = 0
         self.bf = 0
+
+    def bfs(self):
+        q = Queue.Queue()
+        q.put(self)
+        # output_str = ''
+        output_arr = np.zeros((self.height + 1, pow(2, self.height)))
+        i = 0
+        j = 0
+
+        print output_arr.shape
+        # print output_arr[1][1]
+
+        while not q.empty():
+            node = q.get()
+            if node is not None:
+                if node.left is not None:
+                    q.put(node.left)
+                else:
+                    q.put(None)
+                if node.right is not None:
+                    q.put(node.right)
+                else:
+                    q.put(None)
+
+                # Write to 2d array
+                if node is None:
+                    output_arr[i][j] = None
+                else:
+                    output_arr[i][j] = node.key
+
+            # Increment row or col of array as needed
+            if j == pow(2, i) - 1:
+                i += 1
+                j = 0
+            else:
+                j += 1
+
+            # output_str += str(node.key) + ' '
+        return output_arr
 
 
 def insert_node(root, b):
@@ -57,13 +98,28 @@ def right_rotate(root, x):
 def insert(x, z):
     if x is None:
         x = z
-        z.height = x.p
     elif z.key < x.key:
         x.left = insert(x.left, z)
         x.left.parent = x
+        update_height(x)
     else:
         x.right = insert(x.right, z)
+        x.right.parent = x
+        update_height(x)
     return x
+
+
+def update_height(z):
+    while z is not None:
+        lh = 0
+        rh = 0
+        if z.left is not None:
+            lh = z.left.height
+        if z.right is not None:
+            rh = z.right.height
+        if z is not None:
+            z.height = max(lh, rh) + 1
+        z = z.parent
 
 
 def print_tree(root):
@@ -74,6 +130,13 @@ def print_tree(root):
         if root.key == find_max(find_root(root), root.height):
             newline = '\n'
         return str(root.key) + newline + print_tree(root.left) + ' ' + print_tree(root.right)
+
+def print_bfs(z):
+    str = np.zeros((1,np.size(z,0)))
+    for i in range(np.size(z,0)):
+        for j in range(pow(2,i)):
+            str[i] += str(z[i][j]).ljust(pow(np.))
+
 
 
 def find_root(u):
@@ -101,13 +164,20 @@ c = AVLNode(4)
 
 insert(a, b)
 insert(a, c)
+insert(a, AVLNode(1))
 
 # print a.left.key
 # print a.right.key
-print find_max(c, 0)
+# print find_max(c, 0)
 print
 
 print a.height
+print b.height
+print c.height
+
+print c.left.key
+
+print a.bfs()
 
 # print print_tree(a)
 
